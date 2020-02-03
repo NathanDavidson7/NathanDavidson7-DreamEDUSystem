347dae779f510b784f882194e761c38c45e95123
p17165000 <p17165000@my365.dmu.ac.uk>
2020-02-03 17:53:44 +0000

    added course table tests

---
+++ DreamEDU Testing/LiveDateCourse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DreamEDU_Testing
+{
+    [TestClass]
+    public class LiveDateCourse
+    {
+        [TestMethod]
+        public void LiveDataCourseOK()
+        {
+            //create an instance of the class we want to create
+            clsCourses aCourse = new clsCourses();
+            //create some test data to assign to the course
+            DateTime TestData = DateTime.Now.Date;
+            //assign the data to the course
+            aCourse.LiveDate = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(aCourse.LiveDate,TestData);
+        }
+    }
+}
