79495f19447354c6bf898a970e03f0d13d4880d0
Himanshu Abburu <P2503016@my365.dmu.ac.uk>
2020-02-10 17:33:32 +0000

    Commiting in week 20

---
+++ DreamEDUFrontOffice/App_Code/clsstaff.cs
@@ -0,0 +1,6 @@
+﻿public class clsstaff
+{
+    public clsstaff()
+    {
+    }
+}
---
+++ DreamEDUFrontOffice/StaffViewer.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using DreamEDUClasses;
+
+public partial class StaffViewer : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //create new instance of clsStaff
+        clsStaff aStaff = new clsStaff();
+        //get the data from the session object
+        aStaff = (clsStaff)Session["aStaff"];
+        //display the staff number for this entry
+        Response.Write(aStaff.sID);
+
+    }
+}
