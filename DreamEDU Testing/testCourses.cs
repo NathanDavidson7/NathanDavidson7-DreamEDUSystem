eea32a8369b924ef808fddab27c471eb0d174eaa
Nathan Davidson <P17165000@my365.dmu.ac.uk>
2020-03-12 10:50:38 +0000

    Further Validation tests completed

--- DreamEDU Testing/testCourses.cs
+++ DreamEDU Testing/testCourses.cs
@@ -534,5 +534,388 @@ namespace DreamEDU_Testing
             Assert.AreNotEqual(Error, "");
 
         }
+
+        //Category TEST METHODS!
+        [TestMethod]
+        public void CategoryMinLessOne()
+        {
+            //create an instance of the class we want to create
+            clsCourses aCourse = new clsCourses();
+            //string variable to store any error message
+            String Error = "";
+            //create some test data to pass to the method
+            string Category = "";//this should trigger an error
+            //invoke the method
+            Error = aCourse.Valid(Title, Category, Tutor, LiveDate, Price);
+            //test to see that the result is correct
+            Assert.AreNotEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void CategoryMin()
+        {
+            //create an instance of the class we want to create
+            clsCourses aCourse = new clsCourses();
+            //string variable to store any error message
+            String Error = "";
+            //create some test data to pass to the method
+            string Category = "a";//this should be ok
+            //invoke the method
+            Error = aCourse.Valid(Title, Category, Tutor, LiveDate, Price);
+            //test to see that the result is correct
+            Assert.AreEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void CategoryMinPlusOne()
+        {
+            //create an instance of the class we want to create
+            clsCourses aCourse = new clsCourses();
+            //string variable to store any error message
+            String Error = "";
+            //create some test data to pass to the method
+            string Category = "aa";//this should be ok
+            //invoke the method
+            Error = aCourse.Valid(Title, Category, Tutor, LiveDate, Price);
+            //test to see that the result is correct
+            Assert.AreEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void CategoryMaxLessOne()
+        {
+            //create an instance of the class we want to create
+            clsCourses aCourse = new clsCourses();
+            //string variable to store any error message
+            String Error = "";
+            //create some test data to pass to the method
+            string Category = "aaaaaaaaaaaaaaaaaaa";//this should be ok
+            //invoke the method
+            Error = aCourse.Valid(Title, Category, Tutor, LiveDate, Price);
+            //test to see that the result is correct
+            Assert.AreEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void CategoryMax()
+        {
+            //create an instance of the class we want to create
+            clsCourses aCourse = new clsCourses();
+            //string variable to store any error message
+            String Error = "";
+            //create some test data to pass to the method
+            string Category = "aaaaaaaaaaaaaaaaaaaa";//this should be ok
+            //invoke the method
+            Error = aCourse.Valid(Title, Category, Tutor, LiveDate, Price);
+            //test to see that the result is correct
+            Assert.AreEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void CategoryMid()
+        {
+            //create an instance of the class we want to create
+            clsCourses aCourse = new clsCourses();
+            //string variable to store any error message
+            String Error = "";
+            //create some test data to pass to the method
+            string Category = "aaaaaaaaaa";//this should be ok
+            //invoke the method
+            Error = aCourse.Valid(Title, Category, Tutor, LiveDate, Price);
+            //test to see that the result is correct
+            Assert.AreEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void CategoryMaxPlusOne()
+        {
+            //create an instance of the class we want to create
+            clsCourses aCourse = new clsCourses();
+            //string variable to store any error message
+            String Error = "";
+            //create some test data to pass to the method
+            string Category = "aaaaaaaaaaaaaaaaaaaaa";//this should fail
+            //invoke the method
+            Error = aCourse.Valid(Title, Category, Tutor, LiveDate, Price);
+            //test to see that the result is correct
+            Assert.AreNotEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void CategoryExtremeMax()
+        {
+            //create an instance of the class we want to create
+            clsCourses aCourse = new clsCourses();
+            //string variable to store any error message
+            String Error = "";
+            //create some test data to pass to the method
+            string Category = "";
+            Category = Category.PadRight(500, 'a');//this should fail
+            //invoke the method
+            Error = aCourse.Valid(Title, Category, Tutor, LiveDate, Price);
+            //test to see that the result is correct
+            Assert.AreNotEqual(Error, "");
+        }
+
+        //Tutor TEST METHODS!
+        [TestMethod]
+        public void TutorMinLessOne()
+        {
+            //create an instance of the class we want to create
+            clsCourses aCourse = new clsCourses();
+            //string variable to store any error message
+            String Error = "";
+            //create some test data to pass to the method
+            string Tutor = "";//this should trigger an error
+            //invoke the method
+            Error = aCourse.Valid(Title, Category, Tutor, LiveDate, Price);
+            //test to see that the result is correct
+            Assert.AreNotEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void TutorMin()
+        {
+            //create an instance of the class we want to create
+            clsCourses aCourse = new clsCourses();
+            //string variable to store any error message
+            String Error = "";
+            //create some test data to pass to the method
+            string Tutor = "a";//this should be ok
+            //invoke the method
+            Error = aCourse.Valid(Title, Category, Tutor, LiveDate, Price);
+            //test to see that the result is correct
+            Assert.AreEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void TutorMinPlusOne()
+        {
+            //create an instance of the class we want to create
+            clsCourses aCourse = new clsCourses();
+            //string variable to store any error message
+            String Error = "";
+            //create some test data to pass to the method
+            string Tutor = "aa";//this should be ok
+            //invoke the method
+            Error = aCourse.Valid(Title, Category, Tutor, LiveDate, Price);
+            //test to see that the result is correct
+            Assert.AreEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void TutorMaxLessOne()
+        {
+            //create an instance of the class we want to create
+            clsCourses aCourse = new clsCourses();
+            //string variable to store any error message
+            String Error = "";
+            //create some test data to pass to the method
+            string Tutor = "aaaaaaaaaaaaaaaaaaabbbbb";//this should be ok
+            //invoke the method
+            Error = aCourse.Valid(Title, Category, Tutor, LiveDate, Price);
+            //test to see that the result is correct
+            Assert.AreEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void TutorMax()
+        {
+            //create an instance of the class we want to create
+            clsCourses aCourse = new clsCourses();
+            //string variable to store any error message
+            String Error = "";
+            //create some test data to pass to the method
+            string Tutor = "aaaaaaaaaaaaaaaaaaabbbbbb";//this should be ok
+            //invoke the method
+            Error = aCourse.Valid(Title, Category, Tutor, LiveDate, Price);
+            //test to see that the result is correct
+            Assert.AreEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void TutorMaxPlusOne()
+        {
+            //create an instance of the class we want to create
+            clsCourses aCourse = new clsCourses();
+            //string variable to store any error message
+            String Error = "";
+            //create some test data to pass to the method
+            string Tutor = "aaaaaaaaaaaaaaaaaaabbbbbbb";//this should fail
+            //invoke the method
+            Error = aCourse.Valid(Title, Category, Tutor, LiveDate, Price);
+            //test to see that the result is correct
+            Assert.AreNotEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void TutorExtermeMax()
+        {
+            //create an instance of the class we want to create
+            clsCourses aCourse = new clsCourses();
+            //string variable to store any error message
+            String Error = "";
+            //create some test data to pass to the method
+            string Tutor = "";
+            Tutor = Tutor.PadRight(500, 't');//this should fail
+            //invoke the method
+            Error = aCourse.Valid(Title, Category, Tutor, LiveDate, Price);
+            //test to see that the result is correct
+            Assert.AreNotEqual(Error, "");
+        }
+
+        //Price TEST METHODS!
+        [TestMethod]
+        public void PriceExtremeMin()
+        {
+            //create an instance of the class we want to create
+            clsCourses aCourse = new clsCourses();
+            //string variable to store any error message
+            String Error = "";
+            //create a variable to store the test date data
+            Decimal TestPrice;
+            //set TestPrice to a value
+            TestPrice = 00.00m;
+            //change price to -100
+            TestPrice = -100.00m;
+            //convert the decimal variable to a string variable
+            string Price = TestPrice.ToString();
+            //invoke the method
+            Error = aCourse.Valid(Title, Category, Tutor, LiveDate, Price);
+            //test to see that the result is correct
+            Assert.AreNotEqual(Error, "");
+
+        }
+
+        [TestMethod]
+        public void PriceMinLessOne()
+        {
+            //create an instance of the class we want to create
+            clsCourses aCourse = new clsCourses();
+            //string variable to store any error message
+            String Error = "";
+            //create a variable to store the test date data
+            Decimal TestPrice;
+            //set TestPrice to a value
+            TestPrice = 00.00m;
+            //change price to -00.01
+            TestPrice = -01.00m;
+            //convert the decimal variable to a string variable
+            string Price = TestPrice.ToString();
+            //invoke the method
+            Error = aCourse.Valid(Title, Category, Tutor, LiveDate, Price);
+            //test to see that the result is correct
+            Assert.AreNotEqual(Error, "");
+
+        }
+
+        [TestMethod]
+        public void PriceMin()
+        {
+            //create an instance of the class we want to create
+            clsCourses aCourse = new clsCourses();
+            //string variable to store any error message
+            String Error = "";
+            //create a variable to store the test date data
+            Decimal TestPrice;
+            //set TestPrice to a value
+            TestPrice = 00.00m;
+            //convert the decimal variable to a string variable
+            string Price = TestPrice.ToString();
+            //invoke the method
+            Error = aCourse.Valid(Title, Category, Tutor, LiveDate, Price);
+            //test to see that the result is correct
+            Assert.AreEqual(Error, "");
+
+        }
+
+        [TestMethod]
+        public void PriceMinPlusOne()
+        {
+            //create an instance of the class we want to create
+            clsCourses aCourse = new clsCourses();
+            //string variable to store any error message
+            String Error = "";
+            //create a variable to store the test date data
+            Decimal TestPrice;
+            //set TestPrice to a value
+            TestPrice = 00.00m;
+            //change price to 00.01
+            TestPrice = 00.01m;
+            //convert the decimal variable to a string variable
+            string Price = TestPrice.ToString();
+            //invoke the method
+            Error = aCourse.Valid(Title, Category, Tutor, LiveDate, Price);
+            //test to see that the result is correct
+            Assert.AreEqual(Error, "");
+
+        }
+
+        [TestMethod]
+        public void PriceMax()
+        {
+            //create an instance of the class we want to create
+            clsCourses aCourse = new clsCourses();
+            //string variable to store any error message
+            String Error = "";
+            //create a variable to store the test date data
+            Decimal TestPrice;
+            //set TestPrice to a value
+            TestPrice = 00.00m;
+            //change price to 10000.00
+            TestPrice = 10000.00m;
+            //convert the decimal variable to a string variable
+            string Price = TestPrice.ToString();
+            //invoke the method
+            Error = aCourse.Valid(Title, Category, Tutor, LiveDate, Price);
+            //test to see that the result is correct
+            Assert.AreEqual(Error, "");
+
+        }
+
+        [TestMethod]
+        public void PriceMaxPlusOne()
+        {
+            //create an instance of the class we want to create
+            clsCourses aCourse = new clsCourses();
+            //string variable to store any error message
+            String Error = "";
+            //create a variable to store the test date data
+            Decimal TestPrice;
+            //set TestPrice to a value
+            TestPrice = 00.00m;
+            //change price to 10000.01
+            TestPrice = 10000.01m;
+            //convert the decimal variable to a string variable
+            string Price = TestPrice.ToString();
+            //invoke the method
+            Error = aCourse.Valid(Title, Category, Tutor, LiveDate, Price);
+            //test to see that the result is correct
+            Assert.AreNotEqual(Error, "");
+
+        }
+
+        [TestMethod]
+        public void PriceExtremeMax()
+        {
+            //create an instance of the class we want to create
+            clsCourses aCourse = new clsCourses();
+            //string variable to store any error message
+            String Error = "";
+            //create a variable to store the test date data
+            Decimal TestPrice;
+            //set TestPrice to a value
+            TestPrice = 00.00m;
+            //change price to 9999999.99
+            TestPrice = 9999999.99m;
+            //convert the decimal variable to a string variable
+            string Price = TestPrice.ToString();
+            //invoke the method
+            Error = aCourse.Valid(Title, Category, Tutor, LiveDate, Price);
+            //test to see that the result is correct
+            Assert.AreNotEqual(Error, "");
+
+        }
+
     }
 }
